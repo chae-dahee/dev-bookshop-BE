af973f638be79a985dcd75f79409114d60be7bac
dah <cdh010126r@gmail.com>
2024-05-24 12:22:18 +0900

    feat. 도서 전체조회, 개별조회, 카테고리

--- router/books.js
+++ router/books.js
@@ -1,22 +1,13 @@
 const express = require("express");
+const { allBooks, bookDetail } = require("../controller/BookController");
 const router = express.Router();
 
 router.use(express.json());
 
-//전체 도서 조회
-router.get("/", (req, res) => {
-  res.json("전체 도서 조회");
-});
+//전체 도서 조회 + 카테고비별 조회
+router.get("/", allBooks);
 
 //개별 도서 조회
-router.get("/:id", (req, res) => {
-  res.json("개별 도서 조회");
-});
-
-//카테고리별 도서 조회 > 쿼리 스트링
-router.get("/", (req, res) => {
-  //req.query.categoryId
-  res.json("카테고리별 도서 조회");
-});
+router.get("/:id", bookDetail);
 
 module.exports = router;
