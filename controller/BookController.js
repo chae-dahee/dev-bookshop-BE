a8c88b542daed11874dd500f7d239ca262b93197
dah <cdh010126r@gmail.com>
2024-05-26 16:39:03 +0900

    feat. 카테고리별 조회, 신간 여부

--- controller/BookController.js
+++ controller/BookController.js
@@ -1,36 +1,48 @@
 const conn = require("../mariadb"); //db module
 const { StatusCodes } = require("http-status-codes"); //status codes module
 
-//(요약된)전체 도서 조회 + 카테고리별 조회
+//(요약된)전체 도서 조회 + 카테고리별 조회 + 신간 여부
 const allBooks = (req, res) => {
-  let { category_id } = req.query;
-  if (category_id) {
-    let sql = "SELECT * FROM books WHERE category_id = ?";
-    conn.query(sql, category_id, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      if (results.length) return res.status(StatusCodes.OK).json(results[0]);
-      else return res.status(StatusCodes.NOT_FOUND).end();
-    });
-  } else {
-    let sql = "SELECT * FROM books";
-    conn.query(sql, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(StatusCodes.BAD_REQUEST).end();
-      }
-      return res.status(StatusCodes.OK).json(results);
-    });
+  let { category_id, news, limit, currentPage } = req.query;
+
+  //lim,it : page 당 도서 수
+  //currnetPage : 현재 몇 페이지
+  //offset : 0부터 시작~ limit * (currnetPage-1)
+  let offset = limit * (currentPage - 1);
+
+  let sql = "SELECT * FROM books";
+  let values = [];
+  if (category_id && news) {
+    sql +=
+      " WHERE category_id=? and pub_date BETWEEN date_sub(now(), interval 6 month) AND now()";
+    values=[category_id];
+  } else if (category_id) {
+    sql += " WHERE category_id = ?";
+    values=[category_id];
+  } else if (news) {
+    sql +=
+      " WHERE pub_date BETWEEN date_sub(now(), interval 6 month) AND now()";
   }
+  sql += " LIMIT ? OFFSET ?";
+  values.push(parseInt(limit), offset);
+
+  conn.query(sql, values, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results.length) return res.status(StatusCodes.OK).json(results);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
 };
 
 //개별 도서 조회
 const bookDetail = (req, res) => {
   let { id } = req.params;
 
-  let sql = "SELECT * FROM books WHERE id = ?";
+  let sql = `SELECT * FROM books
+  LEFT JOIN category ON books.category_id = category_id
+  WHERE books.id = ?`;
   conn.query(sql, id, (err, results) => {
     if (err) {
       console.log(err);
@@ -41,4 +53,4 @@ const bookDetail = (req, res) => {
   });
 };
 
-module.exports = { allBooks, bookDetail, booksByCategory };
+module.exports = { allBooks, bookDetail };
