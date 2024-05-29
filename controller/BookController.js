9185dc0618d7a7336e8df139b26a5305ea9e4afa
dah <cdh010126r@gmail.com>
2024-05-29 15:17:05 +0900

    feat. 좋아요

--- controller/BookController.js
+++ controller/BookController.js
@@ -10,15 +10,17 @@ const allBooks = (req, res) => {
   //offset : 0부터 시작~ limit * (currnetPage-1)
   let offset = limit * (currentPage - 1);
 
-  let sql = "SELECT * FROM books";
+  let sql = `SELECT *,
+    (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) as likes
+    FROM books`;
   let values = [];
   if (category_id && news) {
     sql +=
       " WHERE category_id=? and pub_date BETWEEN date_sub(now(), interval 6 month) AND now()";
-    values=[category_id];
+    values = [category_id];
   } else if (category_id) {
     sql += " WHERE category_id = ?";
-    values=[category_id];
+    values = [category_id];
   } else if (news) {
     sql +=
       " WHERE pub_date BETWEEN date_sub(now(), interval 6 month) AND now()";
@@ -38,12 +40,18 @@ const allBooks = (req, res) => {
 
 //개별 도서 조회
 const bookDetail = (req, res) => {
-  let { id } = req.params;
+  let { user_id } = req.body;
+  let book_id = req.params.id;
 
-  let sql = `SELECT * FROM books
-  LEFT JOIN category ON books.category_id = category_id
-  WHERE books.id = ?`;
-  conn.query(sql, id, (err, results) => {
+  let sql = `SELECT *
+  , (SELECT count(*) FROM Bookshop.likes WHERE liked_book_id = books.id) as likes
+  ,(SELECT EXISTS (SELECT * FROM likes WHERE user_id = 1 AND liked_book_id=?)) AS liked
+   FROM books
+   LEFT JOIN category ON books.category_id = category.category_id
+   WHERE books.id=?;
+  `;
+  let values = [user_id, book_id, book_id];
+  conn.query(sql, values, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
